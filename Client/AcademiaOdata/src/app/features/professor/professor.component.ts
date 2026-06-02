d38eae514fb43b0faa8bc7026b368d88c7947f04
RafaKowalski <rafaelmk06@gmail.com>
2026-06-01 21:41:28 -0300

    adiciona serviço para buscar todos os professores e apresentar no front-end

--- Client/AcademiaOdata/src/app/features/professor/professor.component.ts
+++ Client/AcademiaOdata/src/app/features/professor/professor.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProfessorService } from 'src/app/services/professorService/professor.service';
 
 @Component({
   selector: 'app-professor',
   templateUrl: './professor.component.html',
   styleUrls: ['./professor.component.scss']
 })
-export class ProfessorComponent {
+export class ProfessorComponent  implements OnInit {
 
+  professores : any[] = [];
+  elements = [
+    { teste1 : 'teste1'},
+    { teste2 : 'teste2'},
+    { teste3 : 'teste3'},
+  ]
+
+  private professorService = inject(ProfessorService);
+
+  ngOnInit(): void {
+    this.professorService.GetAllProfessores().subscribe(professores => {
+      this.professores = professores;
+    });
+
+  }
 }
--- Client/AcademiaOdata/src/app/services/professorService/professor.service.ts
+++ Client/AcademiaOdata/src/app/services/professorService/professor.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfessorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private apiUrl = environment.apiUrl + '/api'
+
+  GetAllProfessores(): Observable<any> {
+    return this.http.get<any[]>(this.apiUrl + '/Professores')
+  }
 }
